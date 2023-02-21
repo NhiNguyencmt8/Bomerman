721a61dc8abea97d77a19829692556897a75f29a
Kohmei Kadoya <ajis.kkadoya@gmail.com>
2023-02-21 17:46:33 -0500

    Save cartpole example

---
+++ teamNN/project2/gameWrapper.py
@@ -0,0 +1,67 @@
+import sys
+import random
+from typing import List, Any
+
+import numpy as np
+import pygame
+from PIL import Image
+
+from traningchracter import TrainingCharacter
+
+sys.path.insert(0, '../../bomberman')
+sys.path.insert(1, '..')
+
+from game import Game
+
+
+class GameWrapper():
+    """ A wrapper for the game, that adds findPossibleAction, and nextStep functions """
+
+    # Public variables
+    gameObj: Game = None
+    mapFile = None
+
+    action_list: list[str | Any] = ["w", "a", "s", "d", "wa", "wd", "sa", "sd", "wb", "wb", "ab", "sb", "db", "wab",
+                                    "wdb", "sab", "sdb",
+                                    "wbb", ""]
+    action_space = my_array = [i for i in range(0, len(action_list))]
+
+    def get_random_action(self):
+        return random.choice(self.action_list)
+
+    # Constuctor
+    def __init__(self, filename):
+        self.mapFile = filename
+        self.reset();
+
+    def reset(self):
+        """ Resets the game """
+        self.gameObj = Game.fromfile(self.mapFile)
+        self.gameObj.add_character(TrainingCharacter("me", "C", 0, 0))
+
+    def getStateImage(self):
+        """ Returns the current state of the game as an image """
+        self.gameObj.display_gui()
+        screen_array = pygame.surfarray.array3d(self.gameObj.screen)
+        pil_image = Image.fromarray(screen_array)
+        pil_image = pil_image.convert("RGB")
+        # pil_image.save("image.jpg")
+
+        downscale_factor = 4
+        new_width = pil_image.width // downscale_factor
+        new_height = pil_image.height // downscale_factor
+        downscaled_image = pil_image.resize((new_width, new_height))
+        # downscaled_image.save("downscaled_image.jpg")
+
+        grayscale_image = downscaled_image.convert("L")
+        # grayscale_image.save("grayscale_image.jpg")
+        return np.array(grayscale_image)
+
+    def nextStep(self, actionString):
+        """ Performs the actionString on the game, and returns the reward """
+        next(iter(self.gameObj.world.characters.values()))[0].setNextAction(actionString)
+        (self.gameObj.world, self.gameObj.events) = self.gameObj.world.next()
+        self.gameObj.display_gui()
+        pygame.time.wait(500)
+        self.gameObj.world.next_decisions()
+        return self.gameObj.world.scores["me"]
--- teamNN/testcharacter.py
+++ teamNN/testcharacter.py
@@ -10,7 +10,8 @@ sys.path.insert(1, '../teamNN')
 from utility import *
 from project1.minimax import *
 
-#Organizing all states
+
+# Organizing all states
 class State(Enum):
     START = 1
     PLACE_BOMB = 2
@@ -25,61 +26,60 @@ class TestCharacter(CharacterEntity):
     stateMachine = State.START
     ai = AI()
 
-    #Function starts here
+    # Function starts here
     def do(self, wrld):
         print("Current State: ", self.stateMachine)
         self.bombCoolDown -= 1
-        #State Machines starts here
+        # State Machines starts here
         match self.stateMachine:
 
-            #Start state -> When game begins, character will jump into this state
+            # Start state -> When game begins, character will jump into this state
             case State.START:
                 self.move(0, 1)
-                #If at corner
+                # If at corner
                 if self.x == 0 and self.y == 2:
                     self.stateMachine = State.PLACE_BOMB
 
-            #Place bomb, dodge and move onto WaitForBomb state
+            # Place bomb, dodge and move onto WaitForBomb state
             case State.PLACE_BOMB:
                 self.place_bomb()
                 self.dodge_bomb_away_from_monster(wrld)
                 self.waitCount = 0
                 self.stateMachine = State.WAIT_FOR_BOMB
 
-            #Wait till the bomb explodes (stay still) then move on the FarFromMonster state
+            # Wait till the bomb explodes (stay still) then move on the FarFromMonster state
             case State.WAIT_FOR_BOMB:
                 self.move(0, 0)
                 self.waitCount += 1
                 if self.waitCount > 1:
                     self.bombCoolDown = 7
                     self.stateMachine = State.FAR_FROM_MONSTER
-            #State where the monster is >= 8 distance from the character
+            # State where the monster is >= 8 distance from the character
             case State.FAR_FROM_MONSTER:
-                #Running minimax, depth = 2
+                # Running minimax, depth = 2
                 self.ai.reccursionDepth = 2
                 self.ai.isExpectimax = False
 
-                #Generate AI move
+                # Generate AI move
                 nextCell = self.ai.get_next_move(wrld)
-                #Perform the move
+                # Perform the move
                 self.move(nextCell[0] - self.x, nextCell[1] - self.y)
                 print("Score of current world", evaluate_state(wrld, character_location(wrld), monster_location(wrld)))
                 print("Selected Move: ", nextCell)
 
-                #If can place bomb -> placebomb
+                # If can place bomb -> placebomb
                 if self.can_place_bomb(nextCell):
                     self.stateMachine = State.PLACE_BOMB
-                if self.can_place_bomb(nextCell):
-                    self.stateMachine = State.PLACE_BOMB
-                #If distance to monster < 8 -> close to monster
+
+                # If distance to monster < 8 -> close to monster
                 if a_star_distance_to_monster(wrld, (nextCell[0], nextCell[1])) < 8:
                     self.stateMachine = State.CLOSE_TO_MONSTER
 
             case State.CLOSE_TO_MONSTER:
-                #Using expectimax
+                # Using expectimax
                 self.ai.reccursionDepth = 3
                 self.ai.isExpectimax = True
-                #If more than 1 monsters -> run
+                # If more than 1 monsters -> run
                 if len(wrld.monsters.values()) > 1:
                     self.ai.isExpectimax = False
                 nextCell = self.ai.get_next_move(wrld)
@@ -107,18 +107,17 @@ class TestCharacter(CharacterEntity):
         return ((location[0] == 6) and (location[1] == 6 or location[1] == 14)) or (
                 (location[0] == 1) and (location[1] == 2 or location[1] == 10))
 
-
     def dodge_bomb_away_from_monster(self, wrld):
         """
         Move away the position where bomb can explode or meet the monster
         """
-        #Score that the bomb can reach -> hit wall or hit monster
+        # Score that the bomb can reach -> hit wall or hit monster
         left_up_score = None
         right_up_score = None
         left_down_score = None
         right_down_score = None
 
-        #Calculating each direction score
+        # Calculating each direction score
         if is_cell_in_range(wrld, self.x + 1, self.y - 1) and is_cell_walkable(wrld, self.x + 1, self.y - 1):
             right_up_score = len(a_star(wrld, (self.x + 1, self.y - 1), monster_location(wrld)))
         if is_cell_in_range(wrld, self.x - 1, self.y - 1) and is_cell_walkable(wrld, self.x - 1, self.y - 1):
@@ -128,11 +127,11 @@ class TestCharacter(CharacterEntity):
         if is_cell_in_range(wrld, self.x - 1, self.y + 1) and is_cell_walkable(wrld, self.x - 1, self.y + 1):
             left_down_score = len(a_star(wrld, (self.x - 1, self.y + 1), monster_location(wrld)))
 
-        #Summarizing up the dodge options to choose the best one
+        # Summarizing up the dodge options to choose the best one
         dodge_options = [left_up_score, right_up_score, left_down_score, right_down_score]
         dodge_options = [x for x in dodge_options if x is not None]
         print(dodge_options)
-        #Return the move based on the best dodge option (which option that brings the most score)
+        # Return the move based on the best dodge option (which option that brings the most score)
         if len(dodge_options) > 0:
             best_move = max(dodge_options)
             if best_move == left_up_score:
